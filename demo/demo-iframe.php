239012e7d0a2f6b621aa309f1758f00caa32cf80
ViciousVictor <muyotofu@gmail.com>
2013-09-12 14:14:04 -0400

    fixed ie bug in demo pages

--- demo/demo-iframe.php
+++ demo/demo-iframe.php
@@ -1,26 +1,32 @@
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
-<title>DOL Language-widget Demo
-</title>
+		<title>DOL Language-widget Demo
+		</title>
 
-<style></style>
-</head>
-<body>
+		<style></style>
+	</head>
+    <!--[if lt IE 7 ]> <body class=" ie6 "> <![endif]-->
+    <!--[if IE 7 ]>    <body class=" ie7 "> <![endif]-->
+    <!--[if IE 8 ]>    <body class=" ie8 "> <![endif]-->
 
-<div>
-<p>
-server side include implementation
-</p>
-	<iframe src="widget.php" name="language-widget-iframe" frameborder="0" height="400" width="220" >Your browser does not support iframes! Update your browser</iframe>
-</div>
+    <!--[if IE 9 ]>    <body class=" ie9 "> <![endif]-->
+    <!--[if (gt IE 9)|!(IE)]><!--> 
+	<body class="">
+	<!--<![endif]--> 
+		<div>
+		<p>
+		server side include implementation
+		</p>
+			<iframe src="widget.php" name="language-widget-iframe" frameborder="0" height="400" width="220" >Your browser does not support iframes! Update your browser</iframe>
+		</div>
 
 
-<?php 
-	include "../complaint-form.html"
-?>
+		<?php 
+			include "../complaint-form.html"
+		?>
 
 
-</body>
+	</body>
 </html>
--- demo/widget.php
+++ demo/widget.php
@@ -2,14 +2,24 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<link rel="stylesheet" type="text/css" href="../css/language-style.css" > 
+		<style>
+			body { margin: 0 }
+		</style>
 	</head>
-	<body style="margin: 0">
-<?php 
-	include "../language-widget.inc"
-?>
-		
-		
-<script src="../js/jquery.js" ></script>
-<script src="../js/expandables.js" ></script>
+    <!--[if lt IE 7 ]> <body class=" ie6 "> <![endif]-->
+    <!--[if IE 7 ]>    <body class=" ie7 "> <![endif]-->
+    <!--[if IE 8 ]>    <body class=" ie8 "> <![endif]-->
+
+    <!--[if IE 9 ]>    <body class=" ie9 "> <![endif]-->
+    <!--[if (gt IE 9)|!(IE)]><!--> 
+	<body class="">
+	<!--<![endif]--> 
+		<?php 
+			include "../language-widget.inc"
+		?>
+				
+				
+		<script src="../js/jquery.js" ></script>
+		<script src="../js/expandables.js" ></script>
 	</body>
 </html>
